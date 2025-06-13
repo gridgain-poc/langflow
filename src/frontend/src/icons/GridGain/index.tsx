4d5381a4ec5b6653fc2b2034ee776ba78a552674
Aditi Sharma <aditi@zettascape.com>
2025-06-13 17:57:41 +0530

    Updated the formatting as per bot - coderabbitai reviews over langflow PR

--- src/frontend/src/icons/GridGain/index.tsx
+++ src/frontend/src/icons/GridGain/index.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from "react";
 import GridGainSVG from "./GridGain";
 
-export const GridGainIcon = forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>(
-  (props, ref) => <GridGainSVG ref={ref} {...props} />
-);
+export const GridGainIcon = forwardRef<
+  SVGSVGElement,
+  React.SVGProps<SVGSVGElement>
+>((props, ref) => <GridGainSVG ref={ref} {...props} />);
